4e848adaed396d7ee5dd771c34ac17c2933bb23c
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-11-01 21:08:27 -0300

    Better types implemented

--- backend/middleware/VerifyToken.ts
+++ backend/middleware/VerifyToken.ts
@@ -1,12 +1,12 @@
 import jwt, { Secret } from "jsonwebtoken";
 
 export const verifyToken = (req: { headers: { [x: string]: any; }; email: string; }, res: { sendStatus: (arg0: number) => void; }, next: () => void) => {
-  const authHeader: any = req.headers['authorization'];
-  const token: any = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
   if(token == null) return res.sendStatus(401);
-  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as Secret, (err: any, decoded: any ) => {
+  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as Secret, (err: any | null, decoded: any) => {
       if(err) return res.sendStatus(403);
       req.email = decoded.email;
       return next();
-  }) as any;
-}
+  }) as void;
+};
