e41e7292bee4672f2c454777d966d41eef822daf
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-11-01 21:10:03 -0300

    Refactoring - most wrong types removed

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -3,12 +3,12 @@ import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/Users";
 import { verifyToken } from "../middleware/VerifyToken";
 import { refreshToken } from "../controllers/RefreshToken";
- 
+
 const router = express.Router();
- 
-router.get('/users', verifyToken as any, getUsers as any);
-router.post('/users', Register as any);
-router.post('/login', Login as any);
+
+router.get('/users', verifyToken as any, getUsers);
+router.post('/users', Register);
+router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
 
